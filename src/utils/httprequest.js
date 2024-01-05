255faccf5b0efe9cf4725ae2be1f3df1a8f0ed0e
whitemousess <thangcoiofficial@gmail.com>
2024-01-06 01:12:02 +0700

    up

--- src/utils/httprequest.js
+++ src/utils/httprequest.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const httpRequest = axios.create({
-  // baseURL: process.env.REACT_NATIVE_BASE_URL,
-  baseURL: `http://192.168.1.2:1407/api`,
+  baseURL: process.env.REACT_NATIVE_BASE_URL,
 });
